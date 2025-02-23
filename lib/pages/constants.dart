19d1132705a0585fc3091ea722ef65d05782af12
Tzigu <andrei.tigaieriu@student.tuiasi.ro>
2025-02-23 18:26:30 +0200

    Version flaw - resolved

--- lib/pages/constants.dart
+++ lib/pages/constants.dart
@@ -2,11 +2,11 @@ var FINAL = 'Iasi - 130 RON - aboname...';
 var Final2 = 'Iasi - 130 RON- abonament 30 d...';
 var qrpage = 'Metropolitan 130 RON 30 de zile';
 
-//var normalw = 350; //adaptare pt ionel
-//var normalh = 325;//adpatare pt ionel
+var normalw = 350; //adaptare pt ionel
+var normalh = 325;//adpatare pt ionel
 
-var normalw = 365;
-var normalh = 325;
+//var normalw = 365;
+//var normalh = 325;
 
 //var FINAL = 'Abonament ELEV- Zon...';
 //var Final2 = 'Abonament ELEV- Zona 1 GR...';
